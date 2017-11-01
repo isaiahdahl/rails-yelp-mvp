ea5c5b17a988562a704e50adf842eba234acf345
isaiah dahl <isaiahdahl@me.com>
2017-11-01 12:37:30 +0000

    generate review and resturant conteroller

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,13 @@
+class RestaurantController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'restaurant/index'
+
+  get 'restaurant/edit'
+
+  get 'restaurant/new'
+
+  get 'restaurant/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
