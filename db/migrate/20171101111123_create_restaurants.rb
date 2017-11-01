ca39cefb817e9fb539aa51bd39fd646fbe7c2f3e
isaiah dahl <isaiahdahl@me.com>
2017-11-01 11:27:59 +0000

    clean up, nothing major

---
+++ db/migrate/20171101111123_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name, null: false
+      t.string :address, null: false
+      t.integer :phone
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
