0cae07df63ad469a5a329facbf86dee115d82144
isaiah dahl <isaiahdahl@me.com>
2017-11-01 11:48:50 +0000

    created fake restaurants with fake reviews

--- db/migrate/20171101114523_create_restaurants.rb
+++ db/migrate/20171101114523_create_restaurants.rb
@@ -3,7 +3,7 @@ class CreateRestaurants < ActiveRecord::Migration[5.1]
     create_table :restaurants do |t|
       t.string :name, null: false
       t.string :address, null: false
-      t.integer :phone
+      t.integer :phone, :limit => 8
       t.string :category
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+puts "creating 20 fake restaurants"
+20.times do
+  resto = Restaurant.new(
+      name: Faker::LordOfTheRings.location,
+      address: Faker::Address.street_address,
+      category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+      phone: Faker::PhoneNumber.phone_number.gsub!(/\D/, '').to_i
+  )
+  resto.save
+end
+puts "finished"
+
+puts "reviewing those restaurants"
+
+100.times do
+  post = Review.new(
+      rating: [1,2,3,4,5].sample,
+      content: Faker::Lorem.paragraphs(10).sample,
+      restaurant_id: (2..19).to_a.sample
+  )
+  post.save
+end
+
+puts "finished"
